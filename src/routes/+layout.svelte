d1d4b8ea75c73466a30532e7c7e2ef8084ff4d3f
Joseph765 <josephchauvin@Josephs-MacBook-Air.local>
2026-01-27 12:23:24 -0600

    added favicons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,8 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
     import "$volt/css/index.css";
 	import "../app.css";
 
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 
 {@render children()}
